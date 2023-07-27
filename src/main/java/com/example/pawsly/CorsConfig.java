24ce594e602aa3aa36c4da64508e5968b41d35cd
young0314 <holly0314@naver.com>
2023-07-27 16:58:27 +0900

    CorsConfig 파일 수정 (*)

--- src/main/java/com/example/pawsly/CorsConfig.java
+++ src/main/java/com/example/pawsly/CorsConfig.java
@@ -7,18 +7,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.0.3:8080/user/signup","https://192.168.0.3:8080/user/signup",
-                        "http://192.168.0.3:8080/user/login","https://192.168.0.3:8080/user/login" ,
-                        "http://192.168.0.3:8080/board/post","https://192.168.0.3:8080/board/post",
-                        "http://192.168.0.3:8080/board/post/{id}","https://192.168.0.3:8080/board/post/{id}"
-                        ,"http://192.168.0.3:8080/upload","https://192.168.0.3:8080/upload")
-                .allowedMethods("GET","POST","PUT","DELETE")
+                .allowedOrigins("*") // 모든 도메인 허용
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .exposedHeaders("Authorization")
                 .allowCredentials(true)
                 .maxAge(3600);
-
     }
+
 }
