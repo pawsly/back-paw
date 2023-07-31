c74c48a710236fbd4e5d197209cca2e057dd64ba
young0314 <holly0314@naver.com>
2023-07-31 23:46:09 +0900

    카카오 url수정

--- src/main/java/com/example/pawsly/OAuth/KaKaoService.java
+++ src/main/java/com/example/pawsly/OAuth/KaKaoService.java
@@ -27,7 +27,7 @@ public class KaKaoService {
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
             sb.append("&client_id=0c32c835e1296a80fcec2d3ea87bf978"); // TODO REST_API_KEY 입력
-            sb.append("&redirect_uri=http://localhost:8080/user/kakao"); // TODO 인가코드 받은 redirect_uri 입력
+            sb.append("&redirect_uri=http://3.39.25.7:8080/user/kakao"); // TODO 인가코드 받은 redirect_uri 입력
             sb.append("&code=" + code);
             bw.write(sb.toString());
             bw.flush();
