a544f9b174625fc0f66f6b9f8bc8d0ff2f587e71
OldtownJazz <dnpqshf@gmail.com>
2023-08-02 06:37:34 +0900

    receiving

---
+++ src/main/java/com/example/pawsly/PawslyApplication.java
@@ -0,0 +1,13 @@
+package com.example.pawsly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawslyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PawslyApplication.class, args);
+    }
+
+}
