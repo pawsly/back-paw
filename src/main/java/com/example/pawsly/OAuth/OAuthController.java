6fdce979fd555d975fea54aee9349bf137dace3c
young0314 <holly0314@naver.com>
2023-08-01 15:30:36 +0900

    KaKaoRestApi수정

--- src/main/java/com/example/pawsly/OAuth/OAuthController.java
+++ src/main/java/com/example/pawsly/OAuth/OAuthController.java
@@ -1,6 +1,7 @@
 package com.example.pawsly.OAuth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,10 @@ public class OAuthController {
     public ResponseEntity<?> kakaoCallback(@RequestParam String code) throws Exception {
         System.out.println(code);
         String access_Token = userService.getKaKaoAccessToken(code);
+        if (access_Token == null) {
+            // access_Token이 null인 경우, 400 Bad Request와 에러 메시지를 클라이언트로 반환
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Kakao access token is null.");
+        }
         userService.createKakaoUser(access_Token);
 
         return ResponseEntity.ok().build();
