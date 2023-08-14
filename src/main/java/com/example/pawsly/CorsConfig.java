a290baa61fecdceefad71aa2cf7de08ed611530d
young0314 <holly0314@naver.com>
2023-08-14 22:27:37 +0900

    쿠키 cors설정4--> @Order(Ordered.HIGHEST_PRECEDENCE) 추가 시큐리티충돌방지

--- src/main/java/com/example/pawsly/CorsConfig.java
+++ src/main/java/com/example/pawsly/CorsConfig.java
@@ -1,15 +1,19 @@
 package com.example.pawsly;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
+        registry
+                .addMapping("/**")
                 .allowedOrigins("http://3.39.25.7:8080/user/login","http://3.39.25.7:8080/user/signup")
                 .allowedMethods("POST","GET","DELETE","PUT","PATCH")
                 .allowCredentials(true)
